5485d0c9f1527e745e386b1380d9f2d31abb50b1
Marta Karbowska <karbowana.marta@gmail.com>
2021-01-22 22:54:43 +0100

    Added menu and refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,20 @@
 import "../scss/style.scss";
 
 (function() {
-  var $slides = document.querySelectorAll('.slide');
-  var $controls = document.querySelectorAll('.slider__control');
+  var $slides = document.querySelectorAll(".slide");
+  var $controls = document.querySelectorAll(".slider__control");
   var numOfSlides = $slides.length;
   var slidingAT = 1300; // sync this with scss variable
   var slidingBlocked = false;
 
   [].slice.call($slides).forEach(function($el, index) {
     var i = index + 1;
-    $el.classList.add('slide-' + i);
+    $el.classList.add("slide-" + i);
     $el.dataset.slide = i;
   });
 
   [].slice.call($controls).forEach(function($el) {
-    $el.addEventListener('click', controlClickHandler);
+    $el.addEventListener("click", controlClickHandler);
   });
 
   function controlClickHandler() {
@@ -22,32 +22,32 @@ import "../scss/style.scss";
     slidingBlocked = true;
 
     var $control = this;
-    var isRight = $control.classList.contains('m--right');
-    var $curActive = document.querySelector('.slide.s--active');
+    var isRight = $control.classList.contains("m--right");
+    var $curActive = document.querySelector(".slide.s--active");
     var index = +$curActive.dataset.slide;
     (isRight) ? index++ : index--;
     if (index < 1) index = numOfSlides;
     if (index > numOfSlides) index = 1;
-    var $newActive = document.querySelector('.slide-' + index);
+    var $newActive = document.querySelector(".slide-" + index);
 
-    $control.classList.add('a--rotation');
-    $curActive.classList.remove('s--active', 's--active-prev');
-    document.querySelector('.slide.s--prev').classList.remove('s--prev');
+    $control.classList.add("a--rotation");
+    $curActive.classList.remove("s--active", "s--active-prev");
+    document.querySelector(".slide.s--prev").classList.remove("s--prev");
     
-    $newActive.classList.add('s--active');
-    if (!isRight) $newActive.classList.add('s--active-prev');
+    $newActive.classList.add("s--active");
+    if (!isRight) $newActive.classList.add("s--active-prev");
     
 
     var prevIndex = index - 1;
     if (prevIndex < 1) prevIndex = numOfSlides;
 
-    document.querySelector('.slide-' + prevIndex).classList.add('s--prev');
+    document.querySelector(".slide-" + prevIndex).classList.add("s--prev");
 
     setTimeout(function() {
-      $control.classList.remove('a--rotation');
+      $control.classList.remove("a--rotation");
       slidingBlocked = false;
     }, slidingAT*0.75);
-  };
+  }
 }());
 
 console.log("Hello world");
